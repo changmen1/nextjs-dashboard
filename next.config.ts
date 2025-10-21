c8c3f9ae4710afc648c358744d506edf191e2e67
changmen1 <zxl17339811909@163.com>
2025-10-21 11:08:08 +0800

    fix:修复报错

--- next.config.ts
+++ next.config.ts
@@ -2,15 +2,12 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  experimental: {
-    // TODO 开启部分预渲染
-    // 'incremental' 值允许为特定路由采用 PPR。
-    // ppr: 'incremental'
-    // cacheComponents: true, // 启用部分预渲染（PPR）
-    images: {
-      remotePatterns: [new URL('http://ghchart.rshah.org/2016rshah')],
-    },
-  }
+  // experimental: {
+  //   // TODO 开启部分预渲染
+  //   // 'incremental' 值允许为特定路由采用 PPR。
+  //   // ppr: 'incremental'
+  //   // cacheComponents: true, // 启用部分预渲染（PPR）
+  // }
 };
 
 export default nextConfig;
