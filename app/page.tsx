e9d8afc048a51775af48acdb3b4c51cb86a71681
changmen1 <zxl17339811909@163.com>
2025-10-21 13:21:31 +0800

    fix:修复移动端样式

--- app/page.tsx
+++ app/page.tsx
@@ -39,9 +39,18 @@ export default function Page() {
         </div>
       </div>
       {/* github */}
-      <div className="box-border border border-[#d0d7de] rounded-md flex flex-col justify-center items-center w-[764px] mx-auto py-2">
-        {/* hideTotalCount  隐藏总数 hideMonthLabels日历标签 */}
-        <GitHubCalendar username="changmen1" fontSize={12} hideTotalCount={true} hideMonthLabels={false} hideColorLegend={false} colorScheme="light" year={2025} blockSize={10} />
+      <div className="overflow-hidden box-border border border-[#d0d7de] rounded-md flex flex-col justify-center items-center w-full md:w-[764px] mx-auto py-2">
+        {/* 保持原组件属性不变 */}
+        <GitHubCalendar
+          username="changmen1"
+          fontSize={12}
+          hideTotalCount={true}
+          hideMonthLabels={false}
+          hideColorLegend={false}
+          colorScheme="light"
+          year={2025}
+          blockSize={10}
+        />
       </div>
       {/* 个人简介 */}
       <div className="text-black tracking-[1px] text-left w-full mx-auto p-0 text-sm font-normal leading-[24px] block">
@@ -55,17 +64,17 @@ export default function Page() {
         <div className="text-[rgba(0,0,0,0.88)] text-start flex items-center gap-x-[6px] text-[15px] font-normal leading-[24px]">
           <svg className="block w-[16px] h-[16px] text-[16px]" focusable="false" xmlns="http://www.w3.org/2000/svg" viewBox="64 64 896 896" fill="currentColor" width="1em" height="1em"><path d="M511.6 76.3C264.3 76.2 64 276.4 64 523.5 64 718.9 189.3 885 363.8 946c23.5 5.9 19.9-10.8 19.9-22.2v-77.5c-135.7 15.9-141.2-73.9-150.3-88.9C215 726 171.5 718 184.5 703c30.9-15.9 62.4 4 98.9 57.9 26.4 39.1 77.9 32.5 104 26 5.7-23.5 17.9-44.5 34.7-60.8-140.6-25.2-199.2-111-199.2-213 0-49.5 16.3-95 48.3-131.7-20.4-60.5 1.9-112.3 4.9-120 58.1-5.2 118.5 41.6 123.2 45.3 33-8.9 70.7-13.6 112.9-13.6 42.4 0 80.2 4.9 113.5 13.9 11.3-8.6 67.3-48.8 121.3-43.9 2.9 7.7 24.7 58.3 5.5 118 32.4 36.8 48.9 82.7 48.9 132.3 0 102.2-59 188.1-200 212.9a127.5 127.5 0 0138.1 91v112.5c.8 9 0 17.9 15 17.9 177.1-59.7 304.6-227 304.6-424.1 0-247.2-200.4-447.3-447.5-447.3z"></path></svg>
           <span className="inline-block min-w-[76px]">GitHub：</span>
-          <a href="https://github.com/changmen1" target="_blank" className="underline text-[15px] font-bold">https://github.com/changmen1</a>
+          <a href="https://github.com/changmen1" target="_blank" className="overflow-auto underline text-[15px] font-bold">https://github.com/changmen1</a>
         </div>
         <div className="text-[rgba(0,0,0,0.88)] text-start flex items-center gap-x-[6px] text-[15px] font-normal leading-[24px]">
           <svg className="block w-[16px] h-[16px] text-[16px]" focusable="false" xmlns="http://www.w3.org/2000/svg" viewBox="64 64 896 896" fill="currentColor" width="1em" height="1em"><path d="M511.6 76.3C264.3 76.2 64 276.4 64 523.5 64 718.9 189.3 885 363.8 946c23.5 5.9 19.9-10.8 19.9-22.2v-77.5c-135.7 15.9-141.2-73.9-150.3-88.9C215 726 171.5 718 184.5 703c30.9-15.9 62.4 4 98.9 57.9 26.4 39.1 77.9 32.5 104 26 5.7-23.5 17.9-44.5 34.7-60.8-140.6-25.2-199.2-111-199.2-213 0-49.5 16.3-95 48.3-131.7-20.4-60.5 1.9-112.3 4.9-120 58.1-5.2 118.5 41.6 123.2 45.3 33-8.9 70.7-13.6 112.9-13.6 42.4 0 80.2 4.9 113.5 13.9 11.3-8.6 67.3-48.8 121.3-43.9 2.9 7.7 24.7 58.3 5.5 118 32.4 36.8 48.9 82.7 48.9 132.3 0 102.2-59 188.1-200 212.9a127.5 127.5 0 0138.1 91v112.5c.8 9 0 17.9 15 17.9 177.1-59.7 304.6-227 304.6-424.1 0-247.2-200.4-447.3-447.5-447.3z"></path></svg>
           <span className="inline-block min-w-[76px]">掘金：</span>
-          <a href="https://juejin.cn/user/2632668054823427" target="_blank" className="underline text-[15px] font-bold">https://juejin.cn/user/2632668054823427</a>
+          <a href="https://juejin.cn/user/2632668054823427" target="_blank" className="overflow-auto underline text-[15px] font-bold">https://juejin.cn/user/2632668054823427</a>
         </div>
         <div className="text-[rgba(0,0,0,0.88)] text-start flex items-center gap-x-[6px] text-[15px] font-normal leading-[24px]">
           <svg className="block w-[16px] h-[16px] text-[16px]" focusable="false" xmlns="http://www.w3.org/2000/svg" viewBox="64 64 896 896" fill="currentColor" width="1em" height="1em"><path d="M511.6 76.3C264.3 76.2 64 276.4 64 523.5 64 718.9 189.3 885 363.8 946c23.5 5.9 19.9-10.8 19.9-22.2v-77.5c-135.7 15.9-141.2-73.9-150.3-88.9C215 726 171.5 718 184.5 703c30.9-15.9 62.4 4 98.9 57.9 26.4 39.1 77.9 32.5 104 26 5.7-23.5 17.9-44.5 34.7-60.8-140.6-25.2-199.2-111-199.2-213 0-49.5 16.3-95 48.3-131.7-20.4-60.5 1.9-112.3 4.9-120 58.1-5.2 118.5 41.6 123.2 45.3 33-8.9 70.7-13.6 112.9-13.6 42.4 0 80.2 4.9 113.5 13.9 11.3-8.6 67.3-48.8 121.3-43.9 2.9 7.7 24.7 58.3 5.5 118 32.4 36.8 48.9 82.7 48.9 132.3 0 102.2-59 188.1-200 212.9a127.5 127.5 0 0138.1 91v112.5c.8 9 0 17.9 15 17.9 177.1-59.7 304.6-227 304.6-424.1 0-247.2-200.4-447.3-447.5-447.3z"></path></svg>
           <span className="inline-block min-w-[76px]">Dev：</span>
-          <a href="https://dev.to/changmen1" target="_blank" className="underline text-[15px] font-bold">https://dev.to/changmen1</a>
+          <a href="https://dev.to/changmen1" target="_blank" className="overflow-auto underline text-[15px] font-bold">https://dev.to/changmen1</a>
         </div>
       </div>
       {/* 教育经历 */}
